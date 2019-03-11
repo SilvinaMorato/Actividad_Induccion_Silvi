9edceeb668b327006af4b736d4599024668f9d46
SilvinaMorato <silvina.morato@mercadolibre.com>
2019-03-11 14:53:11 -0300

    first commit

---
+++ src/main/java/JsonUtils.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
+
+public class JsonUtils {
+
+    public static String toJson(Object o) {
+        return new Gson ().toJson(o);
+    }
+
+    public static ResponseTransformer json() {
+        return JsonUtils::toJson;
+    }
+
+}
